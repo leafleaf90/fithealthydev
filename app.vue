f82bee9bd87c420d7f7faf7b8e04d7ad056c7da2
ViktorL <viktorlovet@hotmail.com>
2023-12-08 12:04:46 +0700

    useHead for SEO

--- app.vue
+++ app.vue
@@ -5,3 +5,15 @@
     </NuxtLayout>
   </div>
 </template>
+<script setup>
+useHead({
+  title: "FitHealth Developer - Life-changing advice for developers",
+  meta: [
+    {
+      name: "description",
+      content:
+        "Best-of-breed advice for developers who want to be healthy and fit.",
+    },
+  ],
+});
+</script>
